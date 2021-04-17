aedca68127f301048ae6c2c7d5546c04322a7600
dong o kim <otwmoo@gmail.com>
2021-04-17 18:12:35 +0900

    temp

--- dist/js/content_script.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";var o=console.log;chrome.runtime.onMessage.addListener((function(e,r,c){e.color?(o("receive color = "+e.color),document.body.style.backgroundColor=e.color,c("Change color to "+e.color)):c("Color is none")}))})();
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,46 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 
 const { log } = console
 
 const Popup = () => {
-  const [count, setCount] = useState(0)
-  const [currentURL, setCurrentURL] = useState<string>()
-
-  useEffect(() => {
-    chrome.browserAction.setBadgeText({ text: count.toString() })
-  }, [count])
-
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      setCurrentURL(tabs[0].url)
-    })
-  }, [])
-
-  const changeBackground = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const tab = tabs[0]
-      if (tab.id) {
-        chrome.tabs.sendMessage(tab.id, {
-          color: '#555555',
-        }, (message) => {
-          log('result message:', message)
-        })
-      }
-    })
-  }
-
   return (
-    <>
-      <ul style={{ minWidth: '700px'}}>
-        <li>Current URL: {currentURL}</li>
-        <li>Current Time: {new Date().toLocaleTimeString()}</li>
-      </ul>
-      <button onClick={() => setCount(count + 1)} style={{ marginRight: '5px' }}>
-        count up
-      </button>
-      <button onClick={changeBackground}>change back</button>
-    </>
+    <div>ok</div>
   )
 }
 
